8b7dd3a24199ccbaa0336091f9804e151261215d
linhtvc <linhtvc@vnpay.vn>
2021-12-13 15:41:16 +0700

    load data cv by js

--- BE/VN/js/index.js
+++ BE/VN/js/index.js
@@ -1,8 +1,11 @@
 var model = {
     profileInfo: {
         avatar: "./images/Untitled.png",
-        fullName: "Trần Văn Chí Linh",
-        position: "Lập trình viên Back End",
+        fullName: "TRẦN VĂN CHÍ LINH",
+        position: "Lập trình viên BackEnd",
+        birthDay: "21/01/1995",
+        phone: "0916.623.321",
+        marriage: "Độc thân",
         email: "tranvanchilinh2101@gmail.com",
         phoneNumber: "0916.623321",
         address: {
@@ -12,19 +15,22 @@ var model = {
         },
         link: [
             {
-                icon: "bx bxl-facebook-square",
+                icon: "bxl-facebook-square",
                 link: "",
-                name: "Facebook"
+                name: "Facebook",
+                color: "blue"
             },
             {
-                icon: "bx bxl-linkedin-square",
+                icon: "bxl-linkedin-square",
                 link: "",
-                name: "Linked"
+                name: "Linked",
+                color: ""
             },
             {
-                icon: "bx bxl-github",
+                icon: "bxl-github",
                 link: "",
-                name: "GitHub"
+                name: "GitHub",
+                color: ""
             }
         ]
     },
@@ -55,7 +61,7 @@ var model = {
                             taskName: "Lọc tin đăng tuyển dụng."
                         },
                         {
-                            taskName: "ìm kiếm ứng viên (cơ bản, nâng cao)."
+                            taskName: "Tìm kiếm ứng viên (cơ bản, nâng cao)."
                         },
                         {
                             taskName: "Thiết kế giao diện CV ứng viên."
@@ -71,13 +77,13 @@ var model = {
                         }
                     ],
                     evaludate: "Hoàn thành task trong thời hạn."
-                }
+                },
             ]
         }
     ],
     education: [
         {
-            time: "09/2015-10/2018",
+            time: "09/2015 - 10/2018",
             degree: "Bằng kỹ sư",
             specialized: "Ứng dụng phần mềm",
             school: "tại trường Cao đẳng nghề TP.HCM"
@@ -87,29 +93,200 @@ var model = {
         {
             icon: "bx bxl-html5",
             name: "HTML5",
-            evaludate: "Tốt"
+            evaludate: "Tốt",
+            isImage: "1",
+            style: "color: #e93e30;"
         },
         {
             icon: "bx bxl-javascript",
             name: "Javascript",
-            evaludate: "Tốt"
+            evaludate: "Tốt",
+            isImage: "1",
+            style: "color: #f7df1e;"
+        },
+        ,
+        {
+            icon: "images/icons/mongoDB.png",
+            name: "MongoDB",
+            evaludate: "Khá",
+            isImage: "0",
+            style: "color: #f7df1e;"
         }
+
     ],
     language: [
         {
             code: "EN",
             name: "English",
-            achievements: "[B2]"
+            achievements: "[B2]",
+            isImage: "1",
+            style: "color: #e93e30;; background-color: blue;"
         }
     ],
     interests: [
         {
             icon: "bx bx-code-alt",
-            name: "Coding"
+            name: "Coding",
+            isImage: "1"
         },
         {
             icon: "bx bx-cycling",
-            name: "Cycling"
+            name: "Cycling",
+            isImage: "1"
         }
     ]
 }
+// alert(model.profileInfo.fullName)
+
+function getElementById(id) {
+    return document.getElementById(id);
+}
+
+
+function loadProfileInfo() {
+    var profileInfoModel = model.profileInfo;
+    getElementById('fullname').innerHTML = profileInfoModel.fullName;
+    getElementById('position').innerHTML = profileInfoModel.position;
+    getElementById('birthday').innerHTML = profileInfoModel.birthDay;
+    getElementById('marriage').innerHTML = profileInfoModel.marriage;
+    getElementById('email').innerHTML = profileInfoModel.email;
+    getElementById('phone').innerHTML = profileInfoModel.phone;
+
+    var address = `<span>${profileInfoModel.address.number}</span>
+    <span>${profileInfoModel.address.district}</span>
+    <span>${profileInfoModel.address.city}</span>`;
+    getElementById('address').innerHTML = address;
+
+    var contentSocialMedia = "";
+    console.log(profileInfoModel.link)
+    profileInfoModel.link.forEach(e => {
+        contentSocialMedia += `
+        <a href="${e.link.length > 0 ? e.link : ''}" target="_blank">
+        <i class='bx ${e.icon}' ${e.color.length > 0 ? 'style="color:' + e.color + '"' : ""}></i></a>
+        `
+    });
+    getElementById('social-media').innerHTML = contentSocialMedia
+}
+
+function loadWorkExperience() {
+    var lstExperience = model.experience;
+    var contentExperience = "";
+    lstExperience.forEach(e => {
+        contentExperience = `
+        <div class="cv-item-content">
+                <div class="date"><i>${e.time}</i></div>
+                <div class="description">
+                    <div class="company-name"><strong>${e.companyName.toLocaleUpperCase()}</strong></div>
+                    <div class="position">
+                        <label><i class='bx bxs-user-account'></i>Vị trí:</label>
+                        <span>${e.position}</span>
+                    </div>
+                    <div class="project-join">
+                        <label> <i class='bx bxs-wallet'></i>Dự án tham gia:</label>
+                        <span>${loadProjectJoin(e.projectJoin)}</span>
+                    </div>
+                    ${loadProjectJoinTask(e.projectJoinDetail)}
+                </div>
+            </div> `
+    })
+    return contentExperience;
+}
+
+function loadProjectJoin(lstProjectJoin) {
+    var contentProjectJoin = "";
+    lstProjectJoin.forEach(e => {
+        contentProjectJoin += `<a href="${e.website}" target="_blank"><i><u> ${e.nameProject} </u></i></a>,`
+    })
+    return contentProjectJoin;
+}
+
+function loadProjectJoinTask(data) {
+    var contentTask = '';
+    data.forEach(e => {
+        contentTask += `
+        <div class="project-join-detail">
+            <div class="project-name cv-item-titel">
+                <h2>${e.projectName}</h2>
+                <div class="line"></div>
+            </div>
+            <div class="integrated-technology">
+                <label> <i class='bx bx-atom'></i>Công nghệ tích hợp:</label> <br>
+                <span>${e.technology}</span>
+            </div>
+            <div class="tool-used">
+                <label><i class='bx bxs-category'></i>Công cụ sử dụng:</label> <br>
+                <span> ${e.toolUsed}</span>
+            </div>
+            <div class="project-task">
+                <label for=""><i class='bx bx-task'></i>Task chính thực hiện:</label>
+                <div class="list-task">
+                    <ul>
+                         ${loadListTask(e.listTask)}
+                    </ul>
+                </div>
+            </div>
+        </div>`
+    })
+    return contentTask;
+}
+
+function loadListTask(data) {
+    var contentListTask = "";
+    data.forEach(e => {
+        contentListTask += `<li>${e.taskName}</li>`
+    })
+    return contentListTask
+}
+
+function loadEducation() {
+    var educationModel = model.education;
+    var contentEducation = "";
+    educationModel.forEach(e => {
+        contentEducation += `
+        <div class="cv-item-content">
+            <div class="date">${e.time}</div>
+            <div class="description">
+                <p><strong>${e.degree}</strong> - ${e.specialized}</p>
+                <p>${e.school}</p>
+            </div>
+        </div>`
+    })
+    return contentEducation;
+}
+
+function loadListSkill() {
+    var skillModel = model.skills;
+    var contentSkill = "";
+    skillModel.forEach(e => {
+        contentSkill += `
+        <div class="item-with-logo">
+            ${(e.isImage === "1")
+                ? `<i class='bx ${e.icon}' ${e.style.length > 0 ? ' style="' + e.style + '"' : ''}></i>`
+                : `<img src="${e.icon}" alt="">`
+            }
+            <label class="name-skill">${e.name}</label> - <label class="evaludate-skill">${e.evaludate}</label>
+        </div>
+        `;
+    })
+    return contentSkill;
+}
+
+function loadLangage() {
+    var languageModel = model.language;
+    var contentLanguage = '';
+    languageModel.forEach(e => {
+        contentLanguage += `
+        <div class="item-with-logo">
+            <div class="language" style="color: white; background-color: blue;">EN</div>
+            <label>English</label> - <label>[B2]</label>
+        </div>`
+    })
+    return contentLanguage;
+}
+
+loadProfileInfo();
+getElementById('list-experience').innerHTML = loadWorkExperience()
+getElementById('education').innerHTML = loadEducation();
+getElementById('skill').innerHTML = loadListSkill()
+getElementById('language').innerHTML = loadLangage()
+
